4c0ada324a975ec15396e7bf92da6ab28a05c006
amdresw <amdresw@gmail.com>
2025-01-30 19:16:45 -0500

    feat: :sparkles: primera entidad "pilots" creada
    Se creó la estructura de los datos de la entidad pilot y se definió su controlador, encargado de las funcionalidades CRUD

---
+++ backend/src/models/pilot.js
@@ -0,0 +1,10 @@
+// Clase Pilot
+class Pilot {
+    constructor(id, name, team, rol, image) {
+        this.id = id;
+        this.name = name;
+        this.team = team;
+        this.rol = rol;
+        this.image = image;
+    }
+}
