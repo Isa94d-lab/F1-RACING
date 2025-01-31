83cafe43945f3263c3ee5366affed48cc0ecb71b
santiagomtello <santiagomtello@gmail.com>
2025-01-30 19:08:45 -0500

    feat: :sparkles: creacion de la mainpage

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+document.getElementById("background-div").addEventListener("mouseover", function () {
+    const video = document.getElementById("background-video");
+    video.style.display = "block";
+    video.play();
+  });
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.getElementById("background-div").addEventListener("mouseout", function () {
+    const video = document.getElementById("background-video");
+    video.pause();
+    video.currentTime = 0;
+    video.style.display = "none";
+  });
